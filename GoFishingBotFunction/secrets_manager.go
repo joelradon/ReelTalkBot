70b0614b0274f744d519c85bc1085fd0f5f58b13
John Hooks <hooksie11@gmail.com>
2024-10-30 22:22:43 -0400

    just some ideas for things

--- GoFishingBotFunction/secrets_manager.go
+++ GoFishingBotFunction/secrets_manager.go
@@ -1,45 +1,34 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
-	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
 )
 
-// FetchSecrets loads secrets from Azure Key Vault
-func FetchSecrets() (map[string]string, error) {
-	keyVaultName := os.Getenv("KEY_VAULT_NAME")
-	if keyVaultName == "" {
-		return nil, fmt.Errorf("KEY_VAULT_NAME environment variable is not set")
+type App struct {
+	TelegramToken  string
+	CQAKey         string
+	CQAEndpoint    string
+	OpenAIKey      string
+	OpenAIEndpoint string
+}
+
+func NewApp() (*App, error) {
+	tt := os.Getenv("TELEGRAM_TOKEN")
+	cKey := os.Getenv("CQA_KEY")
+	cEndpoint := os.Getenv("CQA_ENDPOINT")
+	oaKey := os.Getenv("OPENAI_KEY")
+	oaEndpoint := os.Getenv("OPENAI_ENDPOINT")
+
+	if tt == "" || cKey == "" || cEndpoint == "" || oaKey == "" || oaEndpoint == "" {
+		return nil, fmt.Errorf("all environment variables are required")
 	}
 
-	vaultUrl := fmt.Sprintf("https://%s.vault.azure.net/", keyVaultName)
-
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to obtain a credential: %v", err)
-	}
-
-	client, err := azsecrets.NewClient(vaultUrl, cred, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create secret client: %v", err)
-	}
-
-	secrets := map[string]string{}
-	secretNames := []string{"TELEGRAM-API-TOKEN", "CQA-ENDPOINT", "CQA-API-KEY", "OPENAI-ENDPOINT", "OPENAI-API-KEY"}
-
-	for _, name := range secretNames {
-		resp, err := client.GetSecret(context.TODO(), name, "", nil)
-		if err != nil {
-			log.Printf("Could not retrieve secret %s: %v\n", name, err)
-			continue
-		}
-		secrets[name] = *resp.Value
-	}
-
-	return secrets, nil
+	return &App{
+		TelegramToken:  tt,
+		CQAKey:         cKey,
+		CQAEndpoint:    cEndpoint,
+		OpenAIKey:      oaKey,
+		OpenAIEndpoint: oaEndpoint,
+	}, nil
 }
